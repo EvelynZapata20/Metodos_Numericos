d6ee9511ad810279968739bda33668cc49881e76
SMCARDONAV <92750321+SMCARDONAV@users.noreply.github.com>
2024-04-21 19:07:32 -0500

    Creacion metodo regla falsa, secante, tabla, grafico y descargaen excel

--- app/metodos.py
+++ app/metodos.py
@@ -1,6 +1,8 @@
 import os
 import matlab.engine
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,send_file
+import pandas as pd
+import openpyxl
 
 separador = os.path.sep 
 dir_actual = os.path.dirname(os.path.abspath(__file__))
@@ -8,7 +10,7 @@ dir = separador.join(dir_actual.split(separador)[:-1])+'\matlab'
 
 eng = matlab.engine.start_matlab()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 import numpy as np
 
@@ -29,5 +31,79 @@ def punto_fijo():
         
     return render_template('formulario_pf.html')
 
+@app.route('/secante', methods=['GET', 'POST'])
+def secante():
+    if request.method == 'POST':
+            f= str(request.form['f']) 
+            x0 = float(request.form['x0'])
+            x1 = float(request.form['x1'])  
+            tol = float(request.form['tol'])
+            Terror = int(request.form['Terror'])
+            niter = int(request.form['niter'])
+            
+            eng.addpath(dir)
+
+            respuesta = eng.secante(f, x0, x1, tol, niter, Terror)
+            df = pd.read_csv('tables/tabla_secante.csv')
+            df = df.astype(str)
+            data = df.to_dict(orient='records')
+
+            # Lee el archivo CSV
+            df = pd.read_csv('tables/tabla_secante.csv')
+            # Escribe los datos en un nuevo archivo Excel
+            df.to_excel('tables/tabla_secante.xlsx', index=False) 
+
+            #Gráfica
+            imagen_path = '../static/grafica_secante.png'  # Ruta de la imagen
+            return render_template('resultado_secante.html',respuesta=respuesta, data=data,imagen_path=imagen_path)
+        
+    return render_template('formulario_secante.html')
+
+@app.route('/secante/descargar', methods=['POST'])
+def descargar_archivo():
+    # Ruta del archivo que se va a descargar
+    archivo_path = 'tables/tabla_secante.xlsx'
+
+    # Enviar el archivo al cliente para descargar
+    return send_file(archivo_path, as_attachment=True)
+
+@app.route('/rf', methods=['GET', 'POST'])
+def reglaFalsa():
+    if request.method == 'POST':
+            f= str(request.form['f']) 
+            x0 = float(request.form['x0'])
+            x1 = float(request.form['x1'])  
+            tol = float(request.form['tol'])
+            Terror = int(request.form['Terror'])
+            niter = int(request.form['niter'])
+            
+            eng.addpath(dir)
+
+            respuesta = eng.rf(f, x0, x1, tol, niter, Terror)
+            print(respuesta[0])
+            df = pd.read_csv('tables/tabla_reglaFalsa.csv')
+            df = df.astype(str)
+            data = df.to_dict(orient='records')
+
+            # Lee el archivo CSV
+            df = pd.read_csv('tables/tabla_reglaFalsa.csv')
+            # Escribe los datos en un nuevo archivo Excel
+            df.to_excel('tables/tabla_reglaFalsa.xlsx', index=False) 
+
+            #Gráfica
+            imagen_path = '../static/grafica_reglaFalsa.png'  # Ruta de la imagen
+            return render_template('resultado_reglaFalsa.html',respuesta=respuesta, data=data,imagen_path=imagen_path)
+        
+    return render_template('formulario_reglaFalsa.html')
+
+@app.route('/rf/descargar', methods=['POST'])
+def descargar_archivorf():
+    # Ruta del archivo que se va a descargar
+    archivo_path = 'tables/tabla_reglaFalsa.xlsx'
+
+    # Enviar el archivo al cliente para descargar
+    return send_file(archivo_path, as_attachment=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
---
+++ app/test.bat
@@ -0,0 +1,2 @@
+C:\Users\admin\AppData\Local\Programs\Python\Python38\python.exe C:\Users\smcardonav\Documents\AnalisisNumerico\Metodos_Numericos\app\metodos.py
+pause
